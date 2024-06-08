762c3742b6ca670d48cd7faeb1679b9b0ab6f18e
Prashant Raj <prashant77399@gmail.com>
2024-06-08 17:42:12 +0530

    added login page

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
     res.render("home.ejs");
 });
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+})
 
 
 app.listen(port, (req, res) => {
